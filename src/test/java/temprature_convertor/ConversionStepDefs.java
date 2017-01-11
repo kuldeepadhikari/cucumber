92b44cfe1b0285b1b7b127e049f52bdba51b5826
Kuldeep Adhikari <kuldeepadh@gmail.com>
2017-01-11 11:34:27 -0500

    Added Feature separte folder

--- src/test/java/temprature_convertor/ConversionStepDefs.java
+++ src/test/java/temprature_convertor/ConversionStepDefs.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.MarionetteDriver;
 //import org.openqa.selenium.firefox.MarionetteDriver;
 //import org.openqa.selenium.remote.DesiredCapabilities;
